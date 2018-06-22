51773b712066e13b2a0d5bc91fd87baf6d92d0ec
AnkitKesharwani <ankitk20@users.noreply.github.com>
2018-06-22 13:19:48 +0530

    Add files via upload

--- Algorithms Specialization/Course 4/satisfy.py
+++ Algorithms Specialization/Course 4/satisfy.py
@@ -1,69 +1,87 @@
 from collections import defaultdict
 
-with open('2sat1.txt', 'r') as file:
+with open('2sat2.txt', 'r') as file:
     f = file.readlines()
 
-propcount = int(f.pop(0))
-propdict = defaultdict(set)
+f.pop(0)
+propdict = defaultdict(list)
 proplist = list()
 
-satisfy = True
 for line in f:
 	a, b = tuple(map(int, line.split()))
-	propdict[a].add(b)
+	propdict[a].append(b)
 	proplist.append((a, b))
 
 # print(propdict)
 keylist = list(propdict.keys())
-
+index = 0
+satisfy = True
 while True:
-	key = keylist[0]
-	satisfy = True
-	satisficationcheck = len(keylist)
+	print(index, len(keylist))
+	# print(keylist)
+	if index == len(keylist):
+		break
+	key = keylist[index]
 	if -key in propdict:
-		satisfy = False
-		for elem in propdict[key]:
-			if -elem in propdict[-key]:
-				propdict[key].remove(elem)
-				propdict[-key].remove(-elem)
-			break
-		newset = propdict[key] | propdict[-key]
-		if len(newset) == 0:
-			del propdict[key]
-			del propdict[-key]
-		elif len(propdict[key]) == 0:
-			del propdict[key]
-			propdict[-key] = newset
-		elif len(propdict[-key]) == 0:
-			del propdict[-key]
-			propdict[key] = newset
-		else:
-			newkey = newset.pop()
-			propdict[newkey] = newset
-	else:
-		newset = propdict[key]
-		for elem in propdict[key]:
-			if -elem in propdict:
-				satisfy = False
-				propdict[key].remove(elem)
-				newset = propdict[key] | propdict[-elem]
-				del propdict[-elem]
+		found = False
+		# print(len(propdict[key]), len(propdict[-key]))
+		for i in range(len(propdict[key])):
+			for j in range(len(propdict[-key])):
+				if propdict[key][i] == -propdict[-key][j]:
+					found = True
+					propdict[key].pop(i)
+					propdict[-key].pop(j)
+					break
+			if found:
 				break
 
-		propdict[key] = newset
+		if not found:
+			newkey = propdict[key].pop()
+			newval = propdict[-key].pop()
+			propdict[newkey].append(newval)
+
 		if len(propdict[key]) == 0:
 			del propdict[key]
+		if len(propdict[-key]) == 0:
+			del propdict[-key]
+		i = 0
+		# print('i reset', i)
+	else:
+		found = spfound = False
+		for i in range(len(propdict[key])):
+			if -propdict[key][i] in propdict:
+				found = True
+				otherkey = -propdict[key][i]
+				for j in range(len(propdict[otherkey])):
+					if key == -propdict[otherkey][j]:
+						spfound = True
+						propdict[otherkey].pop(j)
+						propdict[key].pop(i)
+						if len(propdict[key]) == 0:
+							del propdict[key]
+						if len(propdict[otherkey]) == 0:
+							del propdict[otherkey]
+						break
 
-	if len(propdict) == satisficationcheck:
-		satisfy = True
-		break
+				if not spfound:			
+					propdict[key].pop(i)
+					transfer = propdict[otherkey].pop()
+					propdict[key].append(transfer)
+					if len(propdict[otherkey]) == 0:
+						del propdict[otherkey]
+				break
 
+		if not found:
+			# print(index, 'not found')
+			index += 1
+			continue
+		else:
+			i = 0
+			# print('i reset', i)
 
 	# print(propdict)
-
-	if len(propdict) == 0 or satisfy:
-		break
 		
 	keylist = list(propdict.keys())
 
-print(1 if satisfy else 0)
+print(propdict)
+print(1 if len(propdict) else 0)
